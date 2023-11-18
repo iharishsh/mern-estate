077c5f64c124bdaa00ee4e3906632c37d140e975
iharishsh <iharishsh@gmail.com>
2023-11-18 21:17:40 +0530

    signup

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: '/mern-estate',
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
 })
