6685ff1649d810535338be1a576964b62632d69c
iharishsh <iharishsh@gmail.com>
2023-12-03 14:08:42 +0530

    createListing - API

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ dotenv.config();
 import cookieParser from 'cookie-parser';
 import userRouter from './routes/user.route.js'; 
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 
 mongoose
   .connect(process.env.MONGO)
@@ -25,6 +26,7 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
